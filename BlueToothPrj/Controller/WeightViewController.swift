307001842f60c2a94f9da4a4af0a99a2307d18f3
XDEric <eeeric3995@yahoo.com>
2021-01-03 22:15:10 -0500

    changed example data from json file

--- BlueToothPrj/Controller/WeightViewController.swift
+++ BlueToothPrj/Controller/WeightViewController.swift
@@ -4,13 +4,14 @@
 //
 //  Created by EricM on 12/3/20.
 //
-
 import UIKit
+import Charts
 import CoreBluetooth
 
 
 class WeightViewController: UIViewController {
     
+    //MARK: Properties
     var apiClient = ApiClient()
     var summaryData = [Country](){
         didSet{
@@ -21,30 +22,37 @@ class WeightViewController: UIViewController {
     }
     
     var cbManager: CBCentralManager!
-    var weightData = [Weight](){
+//    var weightData = [Weight](){
+//        didSet{
+//            applySnapshot(weight: weightData)
+//        }
+//    }
+    var newData = [Weighted](){
         didSet{
-            applySnapshot(weight: weightData)
+            applySnapshot(weight: newData)
         }
     }
     
-    var exampleData = Weight.init(weight: 6)
-    var exampleData2 = Weight.init(weight: 5)
-    var exampleData3 = Weight.init(weight: 7)
-//    var test: [Weight] = [exampleData.self,exampleData2.self,exampleData.self]
+    //views
+    private var weightCollectionView: UICollectionView!
+    private var myChartView = LineChartView()
     
+    
+//    var exampleData = Weight.init(weight: 6)
+//    var exampleData2 = Weight.init(weight: 5)
+//    var exampleData3 = Weight.init(weight: 7)
+    
+    
+    // sections for collection view
     enum Section {
         case main
     }
     
     //MARK: typealias datasource and snapshot
-    typealias DataSource = UICollectionViewDiffableDataSource<Section,Weight>
-    typealias DatasourceSnapshot = NSDiffableDataSourceSnapshot<Section,Weight>
+    typealias DataSource = UICollectionViewDiffableDataSource<Section,Weighted>
+    typealias DatasourceSnapshot = NSDiffableDataSourceSnapshot<Section,Weighted>
     private var dataSource: DataSource!
     private var snapshot = DatasourceSnapshot()
-    
-    
-    //Mark: Weight CollectionView
-    private var weightCollectionView: UICollectionView!
     
     
     //MARK: Lifecycle
@@ -53,36 +61,70 @@ class WeightViewController: UIViewController {
         //fetchData()
         collectionViewSetup()
         configureDatasource()
-        applySnapshot(weight: [exampleData,exampleData2,exampleData3])
-        cbManager = CBCentralManager(delegate: self, queue: nil, options: .none)
+        loadData()
+        //applySnapshot(weight: [exampleData,exampleData2,exampleData3])
+        //   cbManager = CBCentralManager(delegate: self, queue: nil, options: .none) //bluetooth on device don't work on simulation
+        lineViewChartSetup()
     }
     
+    //MARK: Chart View Setup
+    private func lineViewChartSetup(){
+        myChartView.delegate = self
+        view.addSubview(myChartView)
+        myChartView.pinchZoomEnabled = true
+        myChartView.translatesAutoresizingMaskIntoConstraints = false
+        myChartView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        myChartView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        myChartView.frame = CGRect(x: view.frame.size.width, y: view.frame.size.width, width: 100, height: 100)
+        
+        NSLayoutConstraint.activate([
+            myChartView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myChartView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            myChartView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+            myChartView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            myChartView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        for x in newData{
+            setChartData(xaxis: Double(x.id), yaxis: Double(x.lbs))
+        }
+    }
+    
+    var plots = [ChartDataEntry]()
+    
+    func setChartData(xaxis: Double, yaxis: Double){
+        plots.append(ChartDataEntry(x: xaxis, y: yaxis))
+        
+        let set = LineChartDataSet(entries: plots)
+        set.colors = ChartColorTemplates.joyful()
+        let data = LineChartData(dataSet: set)
+        myChartView.data = data
+    }
+    
+    //MARK: collection view Setup
+    private func collectionViewSetup(){
+        weightCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: collectionLayout())
+        weightCollectionView.delegate = self
+        weightCollectionView.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        weightCollectionView.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        weightCollectionView.register(WeightCell.self, forCellWithReuseIdentifier: WeightCell.cellIdentifier)
+        view.addSubview(weightCollectionView)
+    }
+    
+    //MARK: Layout of collectionView
     private func collectionLayout()-> UICollectionViewLayout{
         //item
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
-        
         //group
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(75))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
         //section
         let section = NSCollectionLayoutSection(group: group)
-        
         //layout
         let layout = UICollectionViewCompositionalLayout(section: section)
         
         return layout
-    }
-    
-    private func collectionViewSetup(){
-        weightCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: collectionLayout())
-        weightCollectionView.delegate = self
-        weightCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        weightCollectionView.backgroundColor = .systemRed
-        weightCollectionView.register(WeightCell.self, forCellWithReuseIdentifier: WeightCell.cellIdentifier)
-        view.addSubview(weightCollectionView)
     }
     
     private func configureDatasource(){
@@ -90,8 +132,7 @@ class WeightViewController: UIViewController {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeightCell.cellIdentifier, for: indexPath) as! WeightCell
             
             cell.textLabel.text = "test"
-            
-            cell.reset()
+            //            cell.reset()
             
             return cell
         })
@@ -108,8 +149,24 @@ class WeightViewController: UIViewController {
         }
     }
     
-    //apply new changes
-    private func applySnapshot(weight: [Weight]){
+    private func loadData(){
+        // just the string for the name of the file
+        guard let pathToJSONFile =
+                Bundle.main.path(forResource: "JsonTestData", ofType: "json") else {fatalError("couldn't Find json file")}
+        print(pathToJSONFile)
+        // is a reference to the ctual location of the json file
+        let url = URL(fileURLWithPath: pathToJSONFile)
+        do{
+            let data = try Data(contentsOf: url)
+            newData = try WeightData.getWeights(from: data).weight
+            // if either try fails the catch will catch both of them
+        } catch{
+            fatalError("couldn't get weight from JSON")
+        }
+    }
+    
+    //MARK: Apply new changes
+    private func applySnapshot(weight: [Weighted]){
         snapshot = DatasourceSnapshot()
         snapshot.appendSections([Section.main])
         snapshot.appendItems(weight)
@@ -119,14 +176,16 @@ class WeightViewController: UIViewController {
 }
 
 
-
-extension WeightViewController : UICollectionViewDelegate{
+//MARK: Extensions
+extension WeightViewController: UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let weight = dataSource.itemIdentifier(for: indexPath) else {return}
-        print(weight.time)
+        print(weight.lbs)
     }
     
 }
+
+extension WeightViewController: ChartViewDelegate{}
 
 
 extension WeightViewController: CBCentralManagerDelegate{
@@ -158,3 +217,5 @@ extension WeightViewController: CBCentralManagerDelegate{
     }
     
 }
+
+
--- BlueToothPrj/Model/WeightJsonDecoder.swift
+++ BlueToothPrj/Model/WeightJsonDecoder.swift
@@ -10,13 +10,24 @@ import Foundation
 // MARK: - WeightData
 struct WeightData: Codable{
     let weight: [Weighted]
-
+    
     enum CodingKeys: String, CodingKey {
         case weight = "Weight"
     }
+    
+    static func getWeights(from data: Data) throws -> WeightData{
+        do{
+            let weights = try JSONDecoder().decode(WeightData.self, from: data)
+            return weights
+        } catch { throw JsonError.decodingError(error)}
+    }
 }
 
-// MARK: - Weight
-struct Weighted: Codable{
+enum JsonError: Error{
+    case decodingError(Error)
+}
+
+// MARK: - Weighted
+struct Weighted: Codable, Hashable{
     let id, lbs: Int
 }
