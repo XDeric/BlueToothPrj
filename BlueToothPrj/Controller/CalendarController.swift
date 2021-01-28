142a55bebee34677a4f7bcceff1abb91a00f12cc
XDEric <eeeric3995@yahoo.com>
2021-01-28 00:33:55 -0500

    adding tableview for calendar

--- BlueToothPrj/Controller/CalendarController.swift
+++ BlueToothPrj/Controller/CalendarController.swift
@@ -11,17 +11,29 @@ import FSCalendar
 class CalendarVC: UIViewController, FSCalendarDelegate, FSCalendarDataSource{
     
     private var calendar = FSCalendar()
+    private var calTableView = UITableView()
+    
+    enum Section {
+        case main
+    }
+    //MARK: typealias datasource and snapshot
+    typealias DataSource = UITableViewDiffableDataSource<Section,SavedWeightData>
+    typealias DatasourceSnapshot = NSDiffableDataSourceSnapshot<Section,SavedWeightData>
+    private var dataSource: DataSource!
+    private var snapshot = DatasourceSnapshot()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //self if this viewcontroller
         calendar.delegate = self
         view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        layoutSetup()
+        calendarSetup()
+        tableViewSetup()
+        twoTaps()
     }
     
     
-    private func layoutSetup(){
+    private func calendarSetup(){
         calendar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(calendar)
         NSLayoutConstraint.activate([
@@ -32,18 +44,34 @@ class CalendarVC: UIViewController, FSCalendarDelegate, FSCalendarDataSource{
         ])
     }
     
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print("selected")
+    private func tableViewSetup(){
+        calTableView.translatesAutoresizingMaskIntoConstraints = false
+        calTableView.backgroundColor = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)
+        calTableView.delegate = self
+        view.addSubview(calTableView)
         
+        NSLayoutConstraint.activate([
+            calTableView.topAnchor.constraint(equalTo: calendar.bottomAnchor),
+            calTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            calTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            calTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
-//    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-//        let weightVC = WeightViewController()
-//        print("se")
-//        
-//        self.navigationController?.pushViewController(weightVC, animated: true)
-//    }
+    private func twoTaps(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+            tap.numberOfTapsRequired = 2
+            calendar.addGestureRecognizer(tap)
+    }
+    @objc func doubleTapped() {
+        let weightVC = WeightViewController()
+        self.navigationController?.pushViewController(weightVC, animated: true)
+    }
     
-    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("selected")
+    }
     
 }
+
+extension CalendarVC: UITableViewDelegate{}
