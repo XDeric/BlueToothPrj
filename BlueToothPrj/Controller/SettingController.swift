876909a634bd5a3a6f5df0a32279bece75a87e56
XDEric <eeeric3995@yahoo.com>
2021-03-09 19:46:36 -0500

    starting work on bluetooth setting tab

--- BlueToothPrj/Controller/SettingController.swift
+++ BlueToothPrj/Controller/SettingController.swift
@@ -9,14 +9,15 @@ import UIKit
 
 class SettingController: UIViewController {
     
-    private var setTableView = UITableView()
+    private var settTableView = UITableView()
+    var btData = [BTData]()
     
     enum Section {
         case main
     }
     //MARK: typealias datasource and snapshot
-    typealias DataSourceTable = UITableViewDiffableDataSource<Section,SavedWeightData>
-    typealias DatasourceSnapshotTable = NSDiffableDataSourceSnapshot<Section,SavedWeightData>
+    typealias DataSourceTable = UITableViewDiffableDataSource<Section,BTData>
+    typealias DatasourceSnapshotTable = NSDiffableDataSourceSnapshot<Section,BTData>
     private var dataSource: DataSourceTable!
     private var snapshot = DatasourceSnapshotTable()
     
@@ -37,7 +38,12 @@ class SettingController: UIViewController {
     }
     
     private func setupConstraints(){
-        
+//        settTableView.translatesAutoresizingMaskIntoConstraints = false
+        settTableView = UITableView(frame: view.bounds)
+        settTableView.delegate = self
+        settTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        settTableView.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        view.addSubview(settTableView)
     }
 
     override func viewDidLoad() {
@@ -46,4 +52,16 @@ class SettingController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: Apply new changes
+    private func applySnapshot(data: [BTData]){
+//        snapshot = DatasourceSnapshot()
+        snapshot.appendSections([Section.main])
+        snapshot.appendItems(data)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+extension SettingController: UITableViewDelegate{
+    
 }
